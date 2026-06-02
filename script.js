3d5ad364abfa0acfc5c544bbcdf5d8857f4a4f8a
Trent-Koessler <trentkoessler@gmail.com>
2026-06-02 17:57:00 +1000

    Update app version to 0.2.0, enhance styling for alert boxes, and modify withdrawal syndrome labels

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const APP_VERSION = '0.1.9';
+    const APP_VERSION = '0.2.0';
     document.querySelectorAll('.app-version').forEach(el => el.textContent = APP_VERSION);
 
     // --- DISCLAIMER MODAL --- //
